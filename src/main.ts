14f037bde93d76a95e60a4763a88b5efc0183522
Christine Yi <christineyi@Christines-MacBook-Pro.local>
2017-02-05 17:00:07 -0500

    didn't work, replacing with example code

--- src/main.ts
+++
@@ -1,6 +0,0 @@
-//enables app to run in the browser
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
